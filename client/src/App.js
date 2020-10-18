f260e9040a3703e5c9ce04dd5d702f9becabd30f
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-10-18 14:16:09 +0100

    added then removed material-ui, as was going to practise, but thought it might waste time, so have added a CSS stylesheet instead to save time

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
+import Navbar from './components/layout/Navbar';
+
 
 function App() {
+  const title = "Address Book";
   return (
+    <BrowserRouter>
     <div className="App">
-      Hello
+     
+       <Navbar title={title} />
+   
     </div>
-  );
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+
+
+const Navbar = ({ title }) => {
+
+    return (
+        <nav className="navbar bg-primary">
+        <h1>
+          <i className={icon}></i> {title}
+        </h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+  
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Navbar
